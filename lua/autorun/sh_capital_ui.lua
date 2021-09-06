9a5200529dc47652ff511f7f162bb85933d41549
imLiaMxo <liammaundrill2015@gmail.com>
2021-09-06 17:47:57 +0100

    Update sh_capital_ui.lua

--- lua/autorun/sh_capital_ui.lua
+++ lua/autorun/sh_capital_ui.lua
@@ -7,11 +7,11 @@ Capital.UI.Version = "0.1-pa"
 hook.Add("Think", "Capital.UI.VersionChecker", function()
 	hook.Remove("Think", "Capital.UI.VersionChecker")
 
-	http.Fetch("VER URL", function(body)
+	http.Fetch("https://raw.githubusercontent.com/imLiaMxo/CapitalUI/main/VERSION?token=ADKF4V47GCMLBN6SYPXKJDLBGZDF6", function(body)
 		if Capital.UI.Version ~= string.Trim(body) then
 			local red = Color(192, 27, 27)
 
-			MsgC(red, "[Capital UI] There is an update available, please download it at: UPDATE HERE\n")
+			MsgC(red, "[Capital UI] There is an update available.\n")
 			MsgC(red, "\nYour version: " .. Capital.UI.Version .. "\n")
 			MsgC(red, "New  version: " .. body .. "\n")
 			return
