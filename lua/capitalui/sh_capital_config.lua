5ecd1e2488226683019aa1303e830aee9f65f87c
imLiaMxo <liammaundrill2015@gmail.com>
2021-09-06 18:19:21 +0100

    added config

---
+++ lua/capitalui/sh_capital_config.lua
@@ -0,0 +1,22 @@
+Capital.UI.Colors = {
+    Background = Color(22, 22, 22),
+    Header = Color(28, 28, 28),
+    Scroller = Color(61, 61, 61),
+
+    PrimaryText = Color(255, 255, 255),
+    SecondaryText = Color(220, 220, 220),
+    DisabledText = Color(40, 40, 40),
+
+    Primary = Color(47, 128, 200),
+    Disabled = Color(180, 180, 180),
+    Positive = Color(66, 134, 50),
+    Negative = Color(164, 50, 50),
+
+    Gold = Color(214, 174, 34),
+    Silver = Color(192, 192, 192),
+    Bronze = Color(145, 94, 49),
+
+    Transparent = Color(0, 0, 0, 0)
+}
+
+Capital.UI.ImgurLoader = "635PPvg" // Imgur ID for the loader.
